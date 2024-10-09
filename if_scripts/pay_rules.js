64c0a79c4f7bb278b7529f8ba8a1dfdb51fde03f
Jalen Battle <jalenbattle78@gmail.com>
2024-10-09 11:56:35 -0400

    Made the if into an if else

--- if_scripts/pay_rules.js
+++ if_scripts/pay_rules.js
@@ -1,6 +1,6 @@
 
 let hours = 20;
-let ot_hours = 0;
+let ot_hours = hours - 40;
 let hours_worked = hours + ot_hours;
 let payByhour = 12.5;
 let pay = (hours * payByhour);
@@ -8,8 +8,12 @@ let money = pay
 
 if (ot_hours > 0) {
     money += (ot_hours *(payByhour * 1.5)) // money += adds this overtime amount to the current value of money, which already contains the regular pay.//
+} else {
+    ot_hours = 0
 }
 
+
+
 console.log(`
-If you worked ${hours_worked} that is ${hours} hours and ${ot_hours} hours of overtime over the last week you will be paid $${money} for the week
+If you worked ${hours} that is ${ot_hours} hours of overtime over the last week you will be paid $${money} for the week
 `)
