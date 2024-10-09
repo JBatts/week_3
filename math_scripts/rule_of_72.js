2ff7044c38e9a62dafb34e8b6b3497e9cd6b07dc
Jalen Battle <jalenbattle78@gmail.com>
2024-10-09 11:14:20 -0400

    Rule 72

---
+++ math_scripts/rule_of_72.js
@@ -0,0 +1,10 @@
+let intersetRate = 3
+let numYears = 72 / intersetRate
+let interest = (19091).toFixed(2)
+
+
+
+
+console.log(`
+ At a ${intersetRate}% inerest rate, your savings account will be worth $${interest} in ${numYears} years   
+`)
