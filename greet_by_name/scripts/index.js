b3c87ea064eacfde38cc872e8811489aa13cf1a9
Jalen Battle <jalenbattle78@gmail.com>
2024-10-10 10:15:47 -0400

    greet by name

---
+++ greet_by_name/scripts/index.js
@@ -0,0 +1,11 @@
+"use strict"
+window.onload = init;
+function init() {
+const greetBtn = document.getElementById("greetBtn");
+greetBtn.onclick = onGreetUserBtnClicked;
+}
+function onGreetUserBtnClicked() {
+const nameField = document.getElementById("nameField");
+let name = nameField.value;
+alert(`Hello there ${name}`)   
+}
